d86be27c372f53e640864f34cf3bc13010e4c317
buldubu <buldubu@gmail.com>
2020-03-28 00:42:52 +0300

    Login Screen

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+/*
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: 'postgres://yfkpgdvcubwpqc:1660f925c79b2b18e82db076d82d21878468405b182641f778147c6aa845080a@ec2-46-137-84-173.eu-west-1.compute.amazonaws.com:5432/d74dbu5iqo08d0',
+//  connectionString: process.env.DATABASE_URL,  
+  ssl: true,
+});
+
+pool.connect();
+
+pool.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  pool.end();
+});
+*/
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,35 @@
 var express = require('express');
 var router = express.Router();
 
-const users = []
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+pool.connect();
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send(users);
+  res.send(pool);
 });
 
 router.post('/user', function(req, res, next) {
   const {username, password} = req.body;
-  users.push({username, password});
-  res.send({success: true});
+  var id = -1;
+  pool.query('SELECT id FROM users where username = $1 AND password = $2;', [username, password], (err, res_db) => {
+    if (err) throw err;
+    for (let row of res_db.rows) {
+      id = row['id'];
+      res.send(JSON.stringify(id))
+    }
+    if(id == -1)res.send(JSON.stringify(id))
+  })
+  
 });
+
+
+
 
 module.exports = router;
