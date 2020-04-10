db4f778ac100d688df9ac4b41e1d9aab602d20d3
buldubu <buldubu@gmail.com>
2020-04-10 20:51:12 +0300

    Cors policy

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var cors = require("cors");
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
@@ -13,6 +14,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(cors());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,11 +23,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 
 
